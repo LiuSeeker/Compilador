e989e562577309c5edd48f6b9fcc9d524701d06f
LiuSeeker <vitor.liu@gmail.com>
2020-04-29 11:49:18 -0300

    roteiro 7

--- compilador.py
+++ compilador.py
@@ -71,6 +71,10 @@ class Tokenizer:
                 self.actual = Token("PVIR", ";")
                 self.position += 1
                 return
+            elif self.origin[self.position] == ".":
+                self.actual = Token("PONT", ".")
+                self.position += 1
+                return
             elif self.origin[self.position] == "=":
                 self.actual = Token("IGUA", "=")
                 self.position += 1
@@ -83,12 +87,32 @@ class Tokenizer:
                 self.position += 1
                 return
             elif self.origin[self.position] == "<":
-                self.actual = Token("MENO", "<")
+                i = self.position
                 self.position += 1
+                if self.position < len(self.origin):
+                    if self.origin[self.position] == " ":
+                        self.actual = Token("MENO", "<")
+                    else:
+                        while self.origin[self.position] != " " and self.origin[self.position] is not "\n":
+                            self.position += 1
+                            if self.position >= len(self.origin):
+                                break
+                        if self.origin[i:self.position] == "<?php":
+                            self.actual = Token("OPRO", self.origin[i:self.position])
+                        else:
+                            raise SyntaxError("Keyword desconhecida {}".format(self.origin[i:self.position]))
                 return
             elif self.origin[self.position] == "!":
                 self.actual = Token("NOT", "!")
                 self.position += 1
+                return
+            elif self.origin[self.position] == "?":
+                i = self.position
+                self.position += 1
+                if self.position < len(self.origin):
+                    if self.origin[self.position] == ">":
+                        self.actual = Token("CPRO", self.origin[i:self.position])
+                        self.position += 1
                 return
             elif self.origin[self.position] == "$":
                 i = self.position
@@ -131,8 +155,25 @@ class Tokenizer:
                 elif self.origin[i:self.position].lower() == "readline":
                     self.actual = Token("READ", self.origin[i:self.position])
                     return
+                elif self.origin[i:self.position].lower() == "true":
+                    self.actual = Token("TRUE", True)
+                    return
+                elif self.origin[i:self.position].lower() == "false":
+                    self.actual = Token("FALSE", False)
+                    return
                 else:
                     raise SyntaxError("Keyword desconhecida {}".format(self.origin[i:self.position]))
+            elif self.origin[self.position] == '"':
+                i = self.position
+                self.position += 1
+                if self.position < len(self.origin):
+                    while self.origin[self.position] != '"':
+                        self.position += 1
+                        if self.position >= len(self.origin):
+                            break
+                    self.actual = Token("STRG", self.origin[i+1:self.position])
+                    self.position += 1
+                return
             
             else:
                 raise SyntaxError("Caractere nao permitido {}".format(self.origin[self.position]))
@@ -143,6 +184,24 @@ class Tokenizer:
 
 class Parser:
     tokens = None
+
+    @staticmethod
+    def parseProgram():
+        if Parser.tokens.actual.type == "OPRO":
+            ret = Comm()
+            Parser.tokens.selectNext()
+            while Parser.tokens.actual.type != "CPRO":
+                #print("NOVA LINHA")
+                if Parser.tokens.actual.type == "EOF":
+                    raise SyntaxError("Line {}: Fechamento ?> esperado".format(Parser.tokens.line_n))
+                ret_t = Parser.parseCommand()
+                if ret_t is not None:
+                    ret.children.append(ret_t)
+            Parser.tokens.selectNext()
+        else:
+            raise SyntaxError("Line {}: Abertura de <?php esperado".format(Parser.tokens.line_n))
+        
+        return ret
 
     @staticmethod
     def parseBlock():
@@ -159,7 +218,7 @@ class Parser:
             Parser.tokens.selectNext()
         else:
             raise SyntaxError("Line {}: Abertura de chaves esperado".format(Parser.tokens.line_n))
-        
+
         return ret
     
     @staticmethod
@@ -224,7 +283,7 @@ class Parser:
             else:
                 raise SyntaxError("Line {}: Abertura de parenteses esperado".format(Parser.tokens.line_n))
         else:
-            raise SyntaxError("Line {}: Identifier, Echo ou Block esperado".format(Parser.tokens.line_n))
+            raise SyntaxError("Line {}: Identifier, Echo, While ou If esperado".format(Parser.tokens.line_n))
 
         return ret
 
@@ -242,7 +301,7 @@ class Parser:
     @staticmethod
     def parseExpression():
         ret = Parser.parseTerm()
-        while Parser.tokens.actual.type == "PLUS" or Parser.tokens.actual.type == "MINUS" or Parser.tokens.actual.type == "OR":
+        while Parser.tokens.actual.type == "PLUS" or Parser.tokens.actual.type == "MINUS" or Parser.tokens.actual.type == "OR" or Parser.tokens.actual.type == "PONT":
             #print("    ",Parser.tokens.actual.type, Parser.tokens.actual.value)
             ret = BinOp(Parser.tokens.actual.value, ret)
             Parser.tokens.selectNext()
@@ -300,7 +359,12 @@ class Parser:
                     raise SyntaxError("Line {}: Fechamento de parenteses esperado apos readline".format(Parser.tokens.line_n))
             else:
                 raise SyntaxError("Line {}: Parenteses esperado apos readline".format(Parser.tokens.line_n))
-        
+        elif Parser.tokens.actual.type == "STRG":
+            ret = StringVal(Parser.tokens.actual.value)
+            Parser.tokens.selectNext()
+        elif Parser.tokens.actual.type == "TRUE" or Parser.tokens.actual.type == "FALSE":
+            ret = BoolVal(Parser.tokens.actual.value)
+            Parser.tokens.selectNext()
         
         elif Parser.tokens.actual.type == "EOF":
             raise SyntaxError("Line {}: Ultimo caractere operador".format(Parser.tokens.line_n))
@@ -322,7 +386,7 @@ class Parser:
         Parser.tokens = Tokenizer(code)
         if Parser.tokens.actual.type == "MULT" or Parser.tokens.actual.type == "DIV":
             raise SyntaxError("Line {}: Primeiro caractere operador nao permitido".format(Parser.tokens.line_n))
-        ast = Parser.parseBlock()
+        ast = Parser.parseProgram()
         if Parser.tokens.actual.type != "EOF":
             if Parser.tokens.actual.type == "CPAR":
                 raise SyntaxError("Line {}: Fechamento de parentes desnecessario".format(Parser.tokens.line_n))
@@ -352,24 +416,74 @@ class BinOp(Node):
         self.children = [c1]
 
     def evaluate(self, st):
+        c1 = self.children[0].evaluate(st)
+        c2 = self.children[1].evaluate(st)
+        c1_int = c1
+        c2_int = c2
+        if c1[0] == "bool":
+            if c1[1] == True:
+                c1_int = ("int", 1)
+            else:
+                c1_int = ("int", 0)
+        if c2[0] == "bool":
+            if c2[1] == True:
+                c2_int = ("int", 1)
+            else:
+                c2_int = ("int", 0)
+        c1_bool = c1
+        c2_bool = c2
+        if c1[0] == "int":
+            if c1[1] == 0:
+                c1_bool = ("bool", False)
+            else:
+                c1_bool = ("bool", True)
+        if c2[0] == "int":
+            if c2[1] == 0:
+                c2_bool = ("bool", False)
+            else:
+                c2_bool = ("bool", True)
+        # aritmeticos
         if self.value == "+":
-            return self.children[0].evaluate(st) + self.children[1].evaluate(st)
+            if c1[0] == "string" or c2[0] == "string":
+                raise TypeError("Line {}: Nao e possivel '+' entre '{}' e '{}'".format(Parser.tokens.line_n, c1[0], c2[0]))
+            return ("int", c1_int[1] + c2_int[1])
         elif self.value == "-":
-            return self.children[0].evaluate(st) - self.children[1].evaluate(st)
+            if c1[0] == "string" or c2[0] == "string":
+                raise TypeError("Line {}: Nao e possivel '-' entre '{}' e '{}'".format(Parser.tokens.line_n, c1[0], c2[0]))
+            return ("int", c1_int[1] - c2_int[1])
         elif self.value == "*":
-            return self.children[0].evaluate(st) * self.children[1].evaluate(st)
+            if c1[0] == "string" or c2[0] == "string":
+                raise TypeError("Line {}: Nao e possivel '*' entre '{}' e '{}'".format(Parser.tokens.line_n, c1[0], c2[0]))
+            return ("int", c1_int[1] * c2_int[1])
         elif self.value == "/":
-            return self.children[0].evaluate(st) / self.children[1].evaluate(st)
-        elif self.value == "==":
-            return self.children[0].evaluate(st) is self.children[1].evaluate(st)
+            if c1[0] == "string" or c2[0] == "string":
+                raise TypeError("Line {}: Nao e possivel '/' entre '{}' e '{}'".format(Parser.tokens.line_n, c1[0], c2[0]))
+            return ("int", c1_int[1] / c2_int[1])
         elif self.value == ">":
-            return self.children[0].evaluate(st) > self.children[1].evaluate(st)
+            if c1[0] == "string" or c2[0] == "string":
+                raise TypeError("Line {}: Nao e possivel '>' entre '{}' e '{}'".format(Parser.tokens.line_n, c1[0], c2[0]))
+            return ("bool", c1_int[1] > c2_int[1])
         elif self.value == "<":
-            return self.children[0].evaluate(st) < self.children[1].evaluate(st)
+            if c1[0] == "string" or c2[0] == "string":
+                raise TypeError("Line {}: Nao e possivel '<' entre '{}' e '{}'".format(Parser.tokens.line_n, c1[0], c2[0]))
+            return ("bool", c1_int[1] < c2_int[1])
+        # booleanos
+        elif self.value == "==":
+            if c1[0] == c2[0] and c1[0] == "string":
+                raise TypeError("Line {}: Nao e possivel '==' entre '{}' e '{}'".format(Parser.tokens.line_n, c1[0], c2[0]))
+            return ("bool", c1_int[1] == c2_int[1])
+        
         elif self.value == "and":
-            return self.children[0].evaluate(st) and self.children[1].evaluate(st)
+            if c1[0] == "string" or c2[0] == "string":
+                raise TypeError("Line {}: Nao e possivel 'and' entre '{}' e '{}'".format(Parser.tokens.line_n, c1[0], c2[0]))
+            return ("bool", c1_bool[1] and c2_bool[1])
         elif self.value == "or":
-            return self.children[0].evaluate(st) or self.children[1].evaluate(st)
+            if c1[0] == "string" or c2[0] == "string":
+                raise TypeError("Line {}: Nao e possivel 'or' entre '{}' e '{}'".format(Parser.tokens.line_n, c1[0], c2[0]))
+            return ("bool", c1_bool[1] or c2_bool[1])
+        # strings
+        elif self.value == ".":
+            return ("string", str(c1[1]) + str(c2[1]))
         else:
             print("BinOp Fail")
 
@@ -379,12 +493,27 @@ class UnOp(Node):
         self.children = []
 
     def evaluate(self, st):
+        c1 = self.children[0].evaluate(st)
+        c1_bool = c1
+        if c1[0] == "int":
+            if c1[1] == 0:
+                c1_bool = ("bool", False)
+            else:
+                c1_bool = ("bool", True)
+        c1_int = c1
+        if c1[0] == "bool":
+            if c1[1] == True:
+                c1_int = ("int", 1)
+            else:
+                c1_int = ("int", 0)
+        if c1[0] == "string":
+            raise TypeError("Line {}: Nao e possivel '{}' com '{}'".format(Parser.tokens.line_n, self.value, c1[0]))
         if self.value == "+":
-            return self.children[0].evaluate(st)
+            return c1_int
         elif self.value == "-":
-            return -self.children[0].evaluate(st)
+            return (c1_int[0], -c1_int[1])
         elif self.value == "!":
-            return not self.children[0].evaluate(st)
+            return (c1_bool[0], not c1_bool[1])
         else:
             print("UnOp Fail")
 
@@ -393,7 +522,21 @@ class IntVal(Node):
         self.value = value
 
     def evaluate(self, st):
-        return self.value
+        return ("int", self.value)
+
+class BoolVal(Node):
+    def __init__(self, value):
+        self.value = value
+
+    def evaluate(self, st):
+        return ("bool", self.value)
+
+class StringVal(Node):
+    def __init__(self, value):
+        self.value = value
+
+    def evaluate(self, st):
+        return ("string", self.value)
 
 class NoOp(Node):
     def evaluate(self, st):
@@ -420,20 +563,29 @@ class Iden(Node):
 
 class Echo(Node):
     def evaluate(self, st):
-        print(int(self.children[0].evaluate(st)))
+        c1 = self.children[0].evaluate(st)
+        if c1[0] == "int":
+            print(int(c1[1]))
+        else:
+            print(c1[1])
+        
 
 class Readline(Node):
     def evaluate(self, st):
-        return input()
+        inp = input()
+        if type(inp) != int:
+            raise TypeError("Line {}: readline tem que ser 'int'".format(Parser.tokens.line_n))
+        return ("int", inp)
 
 class While(Node):    
     def evaluate(self, st):
-        while self.children[0].evaluate(st):
+        while self.children[0].evaluate(st)[1]:
             self.children[1].evaluate(st)
 
 class If(Node):    
     def evaluate(self, st):
-        if self.children[0].evaluate(st):
+        c1 = self.children[0].evaluate(st)
+        if c1[1]:
             self.children[1].evaluate(st)
         else:
             if len(self.children) > 2:
@@ -441,7 +593,7 @@ class If(Node):
 
 class SymbolTable():
     def __init__(self):
-        self.symbols = defaultdict(int)
+        self.symbols = defaultdict(tuple)
     
     def setSymbol(self, symbol, value):
         self.symbols[symbol] = value
@@ -449,7 +601,7 @@ class SymbolTable():
     def getSymbol(self, symbol):
         if symbol not in self.symbols.keys():
             raise NameError("{} nao definido".format(symbol))
-        return int(self.symbols[symbol])
+        return self.symbols[symbol]
 
 
 def main():
--- exemplo-entrada.php
+++ exemplo-entrada.php
@@ -1,14 +1,26 @@
-{
-    $a = readline();
+<?php
+    $st = "oba";
+
+    echo $st;
+
+    $a = 1;
     if ($a < 5)
         $a =  $a - 10;
     else
-        $a =  $a + 10;
+        if ($a == 10)
+            $a = 20;
+        else
+            $a = 30;
     
     echo $a;
+
+    $t = true;
+    $s = 2;
     
-    if(!($a < 0) and (3 == 3))
+    if(!($s < 0) and $t){
         echo 10000;
+        echo "zozo";
+    }
     
     if(($a < 0) or ($a == 6) or ($a == 20))
         echo 20000;
@@ -22,11 +34,12 @@
     
     echo $b; /*3*/
 
+    $f = 1.true.false."abo".$st;
+    echo $f;
+
     $c = -8/4;
     $d = 3 - $c; 
-    {
-        $e = 4/ (1+ 1) *2;
-    }
+    $e = 4/ (1+ 1) *2;
     echo $d; /*5*/
     echo $e; /*4*/
-}
+?>
